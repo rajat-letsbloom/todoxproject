d2a96d3061016b4b284a57dcfd5ee124063d0aab
Rajat Verma <rajatletsbloom@Rajats-MacBook-Air.local>
2024-07-19 11:25:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [allTodos, setAllTodos] = useState([])
+ 
+  const addTodo = () => {
+    if(todo !== ''){
+      setAllTodos(before => {
+        return [{title: todo}, ...before]
+      })
+      setTodo('')
+    }else{
+      alert('Input Cannot be Empty :)')
+    }
+  } 
+
+  console.log(allTodos);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(json=> setAllTodos(json))
+  }, [])
+  
+  const checkTask = (ind, val)=>{
+    console.log(ind, val);
+    // console.log(allTodos)
+    setAllTodos((prev)=>{
+      prev[ind].completed = val === 'on'
+      return prev
+    })
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>TO-DO LIST</h1>
+        <h3>Get your to do list here...</h3>
+
+        <input class="box" type="text" placeholder="to-do list.." id="content" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+
+        <button onClick={addTodo}>Add To The List</button>
+
+{/* {todo} */}
+        {/* <button id="fetch-todos" onclick="getTodos()">Fetch From API</button> */}
+        <div className='dashboard'>
+          <div className='dashboard-items'>
+            <p>Total To-Dos: {allTodos.length} </p>
+          </div>
+          <div className='dashboard-items'>
+            <p>Completed: {allTodos.filter(tasks => tasks.completed).length} </p>
+          </div>
+          <div className='dashboard-items'>
+          <p>Incompleted: {allTodos.filter(tasks => !tasks.completed).length} </p>
+          </div>
+        </div>
+
+        <ul id="parentList">
+          {allTodos?.map((item, index)=>{
+            return <li key={index}>
+              <input type='checkbox' onChange={(e) => checkTask(index, e.target.value)}/>{item.title}
+            <span><i className='fa fa-trash-o'></i></span>
+            </li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
