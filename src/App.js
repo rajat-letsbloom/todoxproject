e3b08353a3809b9485846ae910f22d268cbb12ba
rajat-letsbloom <rajat.verma@letsbloom.io>
2024-07-20 10:44:03 +0530

    NewCommit

--- src/App.js
+++ src/App.js
@@ -6,32 +6,52 @@ import { useState, useEffect } from 'react';
 function App() {
   const [todo, setTodo] = useState('');
   const [allTodos, setAllTodos] = useState([])
- 
-  const addTodo = () => {
-    if(todo !== ''){
-      setAllTodos(before => {
-        return [{title: todo}, ...before]
+
+  
+  const addTodos = () => {
+    if (todo !== '') {
+      fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: todo,
+          // id: allTodos.length + 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
       })
-      setTodo('')
-    }else{
+      .then((response) => response.json())
+      .then((json) => {
+        setAllTodos(before => {
+          return [json, ...before]
+        })
+        setTodo('')
+      })
+    } else {
       alert('Input Cannot be Empty :)')
     }
-  } 
+  }
+
+  const deletTodo = () =>{
+
+  }
 
   console.log(allTodos);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(res => res.json())
-    .then(json=> setAllTodos(json))
+      .then(res => res.json())
+      .then(json => setAllTodos(json))
   }, [])
-  
-  const checkTask = (ind, val)=>{
+
+  const checkTask = (ind, val) => {
     console.log(ind, val);
     // console.log(allTodos)
-    setAllTodos((prev)=>{
-      prev[ind].completed = val === 'on'
-      return prev
+    setAllTodos((prev) => {
+      let tds = [...prev];
+      tds[ind].completed = !tds[ind].completed
+      console.log(tds[ind])
+      return tds
     })
   }
 
@@ -42,11 +62,11 @@ function App() {
         <h1>TO-DO LIST</h1>
         <h3>Get your to do list here...</h3>
 
-        <input class="box" type="text" placeholder="to-do list.." id="content" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+        <input class="box" type="text" placeholder="to-do list.." id="content" value={todo} onChange={(e) => setTodo(e.target.value)} />
 
-        <button onClick={addTodo}>Add To The List</button>
+        <button onClick={addTodos}>Add To The List</button>
 
-{/* {todo} */}
+        {/* {todo} */}
         {/* <button id="fetch-todos" onclick="getTodos()">Fetch From API</button> */}
         <div className='dashboard'>
           <div className='dashboard-items'>
@@ -56,15 +76,16 @@ function App() {
             <p>Completed: {allTodos.filter(tasks => tasks.completed).length} </p>
           </div>
           <div className='dashboard-items'>
-          <p>Incompleted: {allTodos.filter(tasks => !tasks.completed).length} </p>
+            <p>Incompleted: {allTodos.filter(tasks => !tasks.completed).length} </p>
           </div>
         </div>
 
         <ul id="parentList">
-          {allTodos?.map((item, index)=>{
+          {allTodos?.map((item, index) => {
             return <li key={index}>
-              <input type='checkbox' onChange={(e) => checkTask(index, e.target.value)}/>{item.title}
-            <span><i className='fa fa-trash-o'></i></span>
+              <input type='checkbox' checked={item?.completed} onChange={(e) => checkTask(index, e.target.value)} />
+              {item.title}
+              <span><i className='fa fa-trash-o' onClick={e => deletTodo(e.target)}></i></span>
             </li>
           })}
         </ul>
